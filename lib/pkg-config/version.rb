2b4fc9f46483364c127d02a9bf8f3b8bb71e6794
Kouhei Sutou <kou@clear-code.com>
2013-12-30 09:39:02 +0900

    Bump version
    1.1.5 ->
    1.1.6

--- lib/pkg-config/version.rb
+++ lib/pkg-config/version.rb
@@ -1,4 +1,4 @@
-# Copyright 2012 Kouhei Sutou <kou@cozmixng.org>
+# Copyright 2012-2013 Kouhei Sutou <kou@cozmixng.org>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -15,5 +15,5 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 module PKGConfig
-  VERSION = "1.1.5"
+  VERSION = "1.1.6"
 end
