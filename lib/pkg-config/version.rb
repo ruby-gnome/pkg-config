ecfe0f95af44bc12e2c7a12741353638d3b104b5
Sutou Kouhei <kou@clear-code.com>
2020-02-10 11:34:05 +0900

    Bump version

--- lib/pkg-config/version.rb
+++ lib/pkg-config/version.rb
@@ -1,4 +1,4 @@
-# Copyright 2012-2019 Kouhei Sutou <kou@cozmixng.org>
+# Copyright 2012-2020  Sutou Kouhei <kou@cozmixng.org>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -15,5 +15,5 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 module PKGConfig
-  VERSION = "1.4.1"
+  VERSION = "1.4.2"
 end
