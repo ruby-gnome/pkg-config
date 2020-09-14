ff85b61a275b6c15ef47e423edfeb1a6a725df92
Sutou Kouhei <kou@clear-code.com>
2020-09-15 05:48:46 +0900

    Remove needless implicit Bundler

--- test/run-test.rb
+++ test/run-test.rb
@@ -6,9 +6,6 @@ base_dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 lib_dir = File.join(base_dir, "lib")
 test_dir = File.join(base_dir, "test")
 
-ENV["BUNDLE_GEMFILE"] ||= File.join(base_dir, "Gemfile")
-require "bundler/setup"
-
 require 'test-unit'
 
 $LOAD_PATH.unshift(lib_dir)
