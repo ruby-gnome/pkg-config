d99fe599da15fab366d3eb22e23e6e9f117ca27d
Oskar Eichler <oskar@songstats.com>
2026-08-28 03:41:30 +0300

    Strip complete version constraints without joining package names

--- lib/pkg-config.rb
+++ lib/pkg-config.rb
@@ -690,7 +690,7 @@ class PackageConfig
 
   def parse_requires(requires)
     return [] if requires.nil?
-    requires_without_version = requires.gsub(/(?:<|>|<=|>=|=)\s*[\d.a-zA-Z_-]+\s*/, "")
+    requires_without_version = requires.gsub(/(?:!=|<=|>=|<|>|=)\s*[^,\s]+\s*/, " ")
     requires_without_version.split(/[,\s]+/)
   end
 
